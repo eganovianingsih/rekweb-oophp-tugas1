8b82550ddf47c3238bd97b7f8106d924a0b62278
eganovianingsih <eganovia07@mail.unpas.ac.id>
2019-10-10 00:11:07 +0700

    menambahkan file coba.php

--- coba.php
+++ coba.php
@@ -1,16 +1,14 @@
 <?php 
 
 class Coba{
+	}
+	$a = new Coba();
+	$b = new Coba();
+	$c = new Coba();
 
-
-}
-$a = new Coba();
-$b = new Coba();
-$c = new Coba();
-
-var_dump($a);
-var_dump($b);
-var_dump($c);
+	var_dump($a);
+	var_dump($b);
+	var_dump($c);
 
 
 
