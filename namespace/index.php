07e2d04329110b1e1af18895f6d28005a3803488
eganovianingsih <eganovia07@mail.unpas.ac.id>
2019-10-10 00:30:49 +0700

    menambahkan file index.php dalam folder namespace

--- namespace/index.php
+++ namespace/index.php
@@ -2,8 +2,6 @@
 	
 	require_once 'App/init.php';
 	
-	
-
 	// $produk1 = new Komik("Naruto","Masashi Kishimoto","Shonen Jump",30000,100);
 	// $produk2 = new Game("Unceharted","Neil Druckmann","Sony Computer",250000,50);
 	
